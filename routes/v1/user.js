2d2fde72709c0e7d20fc88985fc85e1922ac36fc
Sylvanus <sepehr.haghdoust@gmail.com>
2024-06-25 22:25:46 +0330

    ban users just by admins

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const userContoller = require("./../../controllers/v1/user");
+const authMiddleware = require("./../../middlewares/auth");
+const isAdminMiddleware = require("./../../middlewares/isAdmin");
 const router = express.Router();
 
-router.route("/ban/:id").post(userContoller.banUser);
+router
+  .route("/ban/:id")
+  .post(authMiddleware, isAdminMiddleware, userContoller.banUser);
 
 module.exports = router;
