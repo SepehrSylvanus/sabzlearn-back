9853f5597f39369480575a262c9d02127d003b5e
Sylvanus <sepehr.haghdoust@gmail.com>
2024-06-26 18:40:08 +0330

    remove and update categories

--- controllers/v1/category.js
+++ controllers/v1/category.js
@@ -1,4 +1,5 @@
 const categoryModel = require("../../models/category");
+const { isValidObjectId } = require("mongoose");
 exports.create = async (req, res) => {
   const { title, href } = req.body;
 
@@ -10,5 +11,44 @@ exports.getAll = async (req, res) => {
   const categories = await categoryModel.find({});
   res.json(categories);
 };
-exports.remove = async (req, res) => {};
-exports.update = async (req, res) => {};
+exports.remove = async (req, res) => {
+  const { id } = req.params;
+  const isValidId = isValidObjectId(id);
+
+  if (!isValidId) {
+    return res.status(409).json({
+      message: "Desired id is not correct!",
+    });
+  }
+
+  const deletedCategory = await categoryModel.findOneAndDelete({ _id: id });
+
+  return res.json(deletedCategory);
+};
+exports.update = async (req, res) => {
+  const { id } = req.params;
+  const isValidId = isValidObjectId(id);
+
+  if (!isValidId) {
+    return res.status(409).json({
+      message: "Desired id is not correct!",
+    });
+  }
+  const { title, href } = req.body;
+
+  const updatedCategory = await categoryModel.findOneAndUpdate(
+    { _id: id },
+    {
+      title,
+      href,
+    }
+  );
+
+  if (!updatedCategory) {
+    res.status(404).json({
+      message: "Category not found",
+    });
+  }
+
+  return res.json(updatedCategory);
+};
